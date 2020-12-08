f6bf318d6a8dd8724e5b8149afd2b1ad8fcad0b6
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2020-12-08 14:23:18 +0300

    Lesson 3:Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+ 
+  user = 'OrkunYK';
+
+  items = [
+    { description:"Kahvaltı",action:"No" },
+    { description:"Sinema",action:"No" },
+    { description:"Spor",action:"No" },
+    { description:"Sinema",action:"No" }
+  ]
+
 }
